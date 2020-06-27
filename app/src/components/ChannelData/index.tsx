87a0efbb7efa63716f65bd2b3836e53e2cb815d0
vtorres96 <victorcdc96@gmail.com>
2020-06-27 18:42:00 -0300

    added Channel Data component

---
+++ app/src/components/ChannelData/index.tsx
@@ -0,0 +1,52 @@
+import React, { useRef, useEffect } from "react";
+
+import ChannelMessage, { Mention } from "../ChannelMessage";
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from "./styles";
+
+const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
+  return (
+    <Container>
+      <Messages ref={messagesRef}>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage
+            key={n}
+            author="Victor Torres"
+            date="27/06/2020"
+            content="Recriando interface do discord"
+          />
+        ))}
+
+        <ChannelMessage
+          author="Diego Fernandes"
+          date="21/06/2020"
+          content={
+            <>
+              <Mention>@Victor Torres</Mention>, já assistiu os novos conteúdos
+              da rocketseat?
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
+
+      <InputWrapper>
+        <Input type="text" placeholder="Conversarem #chat-livre" />
+        <InputIcon />
+      </InputWrapper>
+    </Container>
+  );
+};
+
+export default ChannelData;
